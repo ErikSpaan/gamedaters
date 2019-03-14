42641475c0a082cb8b0db7a91f5a2cc186061d4f
Philip Andes <philip__andes@hotmail.nl>
2019-03-14 10:01:08 +0100

    code cleanup

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -11,31 +11,32 @@
 <!--Name / logo landingpage -->
 {{-- @section('name/logo') --}}
 
-<!-- left side landingpage -->
+<!-- left side landingpage flex1 top-->
 <div class="container">
     <div class="flex1">
         <div class="flex1_top">
             <img src="/images/logo.jpg" alt="player 2" />
         </div>
+        <!-- left side landingpage flex1 bottom-->
         <div class="flex1_bottom">
            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, optio sint. Iste iusto non quam doloremque voluptate eos dolorem voluptatem atque voluptatibus optio odio, praesentium enim dicta corrupti aliquam. Molestias eveniet obcaecati enim placeat quasi, quo molestiae, nihil eum et dolores nam magni quaerat rem praesentium iure non error tempora?</p>
         </div>
     </div>
 
-    <!--Login flex2 top-->
+    <!--right side landingpage Login flex2 top-->
     <div class="flex2">
         <div class="flex2_top">
             @section('login')
             @include ('codeincludes/login')
         </div>
 
-        <!--Register form flex2 middle-->
+        <!--right side landingpage Register form flex2 middle-->
         <div class="flex2_middle">
                 @section('registerform')
                 @include ('codeincludes/register')
         </div>
 
-        <!--flex2 bottom-->
+        <!--right side landingpage flex2 bottom-->
         <div class="flex2_bottom">   
         </div>
     </div>
