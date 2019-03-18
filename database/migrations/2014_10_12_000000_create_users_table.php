434fc623ab083ae65dada3e3b2b161e455c07b8f
ErikSpaan <email2erik@spamus.nl>
2019-03-18 11:08:41 +0100

    added gamecontroller 18-3-2019 11:08 ES

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name',40);
+            $table->string('email',60)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password',30);
             $table->rememberToken();
-            $table->adminrights()->nullable;
+            $table->string('adminrights',20)->nullable;
             $table->timestamps();
         });
     }
