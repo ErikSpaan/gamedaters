ea8f3aa5da83cf4bf007c3492ec61292387f59c5
RickButer <buter.rick@gmail.com>
2019-03-19 16:25:31 +0100

    commit to save my work. dinsdag 16:25

--- resources/views/codeincludes/profile.blade.php
+++ resources/views/codeincludes/profile.blade.php
@@ -5,7 +5,7 @@
 <div class="profile_container">
     <div class="profile_boxes debug">
         <div class="photo_button_container">
-            <div class="photo_box debug"></div>
+            <div class="photo_box" id="photo_holder"></div>
             <input type="file" class="upload_button" placeholder="upload img">
         </div>
         <div class="input_container">
@@ -13,30 +13,56 @@
             <input type="text" name="" placeholder="first name" class="input_profile_page">
             <input type="text" name="" placeholder="last name" class="input_profile_page">
             <input type="email" name="" placeholder="email" class="input_profile_page">
-            <input type="password" name="" placeholder="password" class="input_profile_page"><br />
+            <input type="password" name="" placeholder="password" class="input_profile_page">
+            <span class="separate_span">gender</span>
             <div class="input_flex">
-                <input type="radio" name="gender" value="male" class="input_radio"><span>male</span>
+                <input type="radio" name="gender" value="male" class="input_radio"><span class="gender_span">male</span>
             </div>
             <div class="input_flex">
-                <input type="radio" name="gender" value="female" class="input_radio"><span>female</span>
+                <input type="radio" name="gender" value="female" class="input_radio"><span class="gender_span">female</span>
             </div>
-            <div class="input_flex"> <input type="radio" name="gender" value="gender neutral" class="input_radio"><span>gender
+            <div class="input_flex"> <input type="radio" name="gender" value="gender neutral" class="input_radio"><span
+                    class="gender_span">gender
                     neutral</span>
-            </div>
+            </div><br />
             <span class="separate_span">age</span>
             <input type="range" name="age" min="18" max="99" placeholder="" class="input_range">
         </div>
         <div class="who_am_i">
             <textarea class="text_area" placeholder="write something about you..."></textarea>
             <div class="see_personal_page">
-                <button class="button_personal_view">see personal page</button>
+                <button class="button_personal_view">see my personal page</button>
+                <button class="button_save_changes">save my changes</button>
 
             </div>
         </div>
-
     </div>
 
+    <div class="profile_boxes">
+        <div class="filter_flex">
+            <div class="filter_name">I'm looking for...</div>
+            <div class="filter_options debug">
+                <span class="separate_span">gender</span>
+                <div class="input_radio_box">
+                    {{-- to change the radio buttons here for responsive, change the width in class: input_radio_box
+                    !!!!! --}}
+                    <input type="radio" name="gender" value="male" class="input_radio">male
 
-    <div class="profile_boxes debug"></div>
-    <div class="profile_boxes debug"></div>
+                    <input type="radio" name="gender" value="female" class="input_radio">female
+
+                    <input type="radio" name="gender" value="gender neutral" class="input_radio">gender neutral
+                </div><br />
+                <span class="separate_span">age</span>
+                <div><input type="range" name="age" min="18" max="99" placeholder="" class="filter_age"></div><br />
+                <span class="separate_span">distance</span>
+                <div><input type="range" name="distance" min="0" max="500" placeholder="" class="filter_age"></div><br />
+                <span class="separate_span">game genre</span>
+            </div>
+            <div class="find_match_button_box">
+                <button class="find_matches_button">Find your match!</button>
+            </div>
+        </div>
+        <div class="matches_flex debug"></div>
+    </div>
+    <div class="profile_boxes"></div>
 </div>
