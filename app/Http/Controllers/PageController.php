c76061834a45ec1346050ec2f222e2574f0e32b8
ErikSpaan <email2erik@spamus.nl>
2019-03-20 16:01:02 +0100

    dates is working and personalpage is create for new user

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -21,18 +21,26 @@ class PageController extends Controller
     }
 
     public function profilepage() {
-        $personalpage = Personalpage::where("user_id",Auth::user()->id)->first();
-        return view('profilepage', compact('personalpage'));
+
+        if (Personalpage::where("user_id",Auth::user()->id)->first()) {
+            $personalpage = Personalpage::where("user_id",Auth::user()->id)->first();
+        } else {
+            $personalpage = new Personalpage();
+            $personalpage->user_id = Auth::user()->id;
+            $personalpage->save();
+            $personalpage = Personalpage::where("user_id",Auth::user()->id)->first();  
+         }
+
+         return view('profilepage', compact('personalpage'));
 
     }
 
     public function personalpage() {
-        // $personalpage = Personalpage::findOrFail(Auth::user()->id);
+        // $personalpage = Personalpage::findOrFail(Auth::user()->id->first());
         // $personalpage = Personalpage::findOrFail(1);
         $personalpage = Personalpage::where("user_id",Auth::user()->id)->first();
         
             // dd($personalpages);
-                
        
         return view('personalpage', compact('personalpage'));
     }
--- app/Personalpage.php
+++ app/Personalpage.php
@@ -9,7 +9,7 @@ class personalpage extends Model
     
     public function users()
     {
-        return $this->belongsToMany('App\User');
+        return $this->belongsToMany('App\User')->withTimestamps();
     }
 
 
