bef7b146cb88dcad16bf94ec5d74039283001bcc
RickButer <buter.rick@gmail.com>
2019-03-20 11:10:49 +0100

    profile almost ready for input rick wednesday 11:10

--- resources/views/codeincludes/profile.blade.php
+++ resources/views/codeincludes/profile.blade.php
@@ -1,7 +1,7 @@
 <head>
     <link rel="stylesheet" href="{{ asset('/css/profile_personalpage.css') }}">
 </head>
-
+{{-- start profile box 1 --}}
 <div class="profile_container">
     <div class="profile_boxes debug">
         <div class="photo_button_container">
@@ -22,8 +22,7 @@
                 <input type="radio" name="gender" value="female" class="input_radio"><span class="gender_span">female</span>
             </div>
             <div class="input_flex"> <input type="radio" name="gender" value="gender neutral" class="input_radio"><span
-                    class="gender_span">gender
-                    neutral</span>
+                    class="gender_span">gender neutral</span>
             </div><br />
             <span class="separate_span">age</span>
             <input type="range" name="age" min="18" max="99" placeholder="" class="input_range">
@@ -31,17 +30,17 @@
         <div class="who_am_i">
             <textarea class="text_area" placeholder="write something about you..."></textarea>
             <div class="see_personal_page">
-                <button class="button_personal_view">see my personal page</button>
+                <button class="button_personal_view">my personal page</button>
                 <button class="button_save_changes">save my changes</button>
 
             </div>
         </div>
     </div>
-
+    {{-- Start profile box 2 --}}
     <div class="profile_boxes">
         <div class="filter_flex">
             <div class="filter_name">I'm looking for...</div>
-            <div class="filter_options debug">
+            <div class="filter_options">
                 <span class="separate_span">gender</span>
                 <div class="input_radio_box">
                     {{-- to change the radio buttons here for responsive, change the width in class: input_radio_box
@@ -51,18 +50,93 @@
                     <input type="radio" name="gender" value="female" class="input_radio">female
 
                     <input type="radio" name="gender" value="gender neutral" class="input_radio">gender neutral
-                </div><br />
+                </div>
                 <span class="separate_span">age</span>
-                <div><input type="range" name="age" min="18" max="99" placeholder="" class="filter_age"></div><br />
+                <div><input type="range" name="age" min="18" max="99" placeholder="" class="filter_age"></div>
                 <span class="separate_span">distance</span>
-                <div><input type="range" name="distance" min="0" max="500" placeholder="" class="filter_age"></div><br />
+                <div><input type="range" name="distance" min="0" max="500" placeholder="" class="filter_age"></div>
                 <span class="separate_span">game genre</span>
-            </div>
-            <div class="find_match_button_box">
-                <button class="find_matches_button">Find your match!</button>
+                <div class="genre_box">
+                    <input list="genres" name="genre" placeholder="choose a genre" class="list_dropdown">
+                    <datalist id="genres">
+                        <option value="role playing game">
+                        <option value="shooter">
+                        <option value="action adventure">
+                        <option value="online">
+                        <option value="sandbox">
+                        <option value="battle royale">
+                        <option value="fighting">
+                        <option value="strategy">
+                        <option value="horror">
+                    </datalist>
+                    <button class="find_matches_button">Find your match!</button>
+                </div>
             </div>
         </div>
-        <div class="matches_flex debug"></div>
+        <div class="matches_flex">
+            <div class="matches_card">
+                <a href="#" class="card_photo_box debug"><img src="" alt="" /></a>
+                <div class="card_name_box debug"></div>
+                <div id="card_button_box"></div>
+            </div>
+            <div class="matches_card">
+                <a href="#" class="card_photo_box debug"><img src="" alt="" /></a>
+                <div class="card_name_box debug"></div>
+                <div id="card_button_box"></div>
+            </div>
+            <div class="matches_card">
+                <a href="#" class="card_photo_box debug"><img src="" alt="" /></a>
+                <div class="card_name_box debug"></div>
+                <div id="card_button_box"></div>
+            </div>
+            <div class="matches_card">
+                <a href="#" class="card_photo_box debug"><img src="" alt="" /></a>
+                <div class="card_name_box debug"></div>
+                <div id="card_button_box"></div>
+            </div>
+            <div class="matches_card">
+                <a href="#" class="card_photo_box debug"><img src="" alt="" /></a>
+                <div class="card_name_box debug"></div>
+                <div id="card_button_box"></div>
+            </div>
+        </div>
     </div>
-    <div class="profile_boxes"></div>
+    {{-- Start profile box 3 --}}
+    <div class="profile_boxes">
+        <div class="current_dates_container">
+            <div class="filter_name">Current dates</div>
+            <div class="current_dates_box">
+                <div class="matches_card">
+                    <a href="#" class="card_photo_box debug"><img src="" alt="" /></a>
+                    <div class="card_name_box debug"></div>
+                    <div id="card_delete_box"></div>
+                </div>
+                <div class="matches_card">
+                    <a href="#" class="card_photo_box debug"><img src="" alt="" /></a>
+                    <div class="card_name_box debug"></div>
+                    <div id="card_delete_box"></div>
+                </div>
+                <div class="matches_card">
+                    <a href="#" class="card_photo_box debug"><img src="" alt="" /></a>
+                    <div class="card_name_box debug"></div>
+                    <div id="card_delete_box"></div>
+                </div>
+                <div class="matches_card">
+                    <a href="#" class="card_photo_box debug"><img src="" alt="" /></a>
+                    <div class="card_name_box debug"></div>
+                    <div id="card_delete_box"></div>
+                </div>
+                <div class="matches_card">
+                    <a href="#" class="card_photo_box debug"><img src="" alt="" /></a>
+                    <div class="card_name_box debug"></div>
+                    <div id="card_delete_box"></div>
+                </div>
+            </div>
+
+        </div>
+        <div class="games_container">
+    </div>
+
+    </div>
+
 </div>
