8a7e199944263826066d89435c48e034dd20d703
RickButer <buter.rick@gmail.com>
2019-03-21 21:08:36 +0100

    everything pulled and working again. 21-03-2019 21:08

--- resources/views/codeincludes/profile.blade.php
+++ resources/views/codeincludes/profile.blade.php
@@ -12,28 +12,43 @@
         <form method="POST" action="/personalpages/{{ $personalpage->id }}">
             {{ method_field('PATCH') }}
             {{-- to protect against cross site... --}}
-            {{ csrf_field() }}  
+            {{ csrf_field() }}
             <div class="input_container">
-                <input type="text" name="personal_firstname" placeholder="first name" value="{{ $personalpage->personal_firstname }}" class="input_profile_page">
-                <input type="text" name="personal_lastname" placeholder="last name" value="{{ $personalpage->personal_lastname }}" class="input_profile_page">
-                <input type="text" name="user_nickname" placeholder="nickname" value="{{ Auth::user()->name }}" class="input_profile_page">
-                <input type="email" name="user_email" placeholder="email" value="{{ Auth::user()->email }}" class="input_profile_page">
+                <input type="text" name="personal_firstname" placeholder="first name"
+                    value="{{ $personalpage->personal_firstname }}" class="input_profile_page">
+                <input type="text" name="personal_lastname" placeholder="last name"
+                    value="{{ $personalpage->personal_lastname }}" class="input_profile_page">
+                <input type="text" name="user_nickname" placeholder="nickname" value="{{ Auth::user()->name }}"
+                    class="input_profile_page">
+                <input type="email" name="user_email" placeholder="email" value="{{ Auth::user()->email }}"
+                    class="input_profile_page">
                 <input type="password" name="user_password" placeholder="password" value="" class="input_profile_page">
                 <span class="separate_span">gender</span>
                 <div class="input_flex">
-                    <input type="radio" name="personal_gender" {{ ($personalpage->personal_gender=='male') ? 'checked="checked" ' : '' }} value="male" class="input_radio"><span class="gender_span">male</span>
+                    <input type="radio" name="personal_gender"
+                        {{ ($personalpage->personal_gender=='male') ? 'checked="checked" ' : '' }} value="male"
+                        class="input_radio"><span class="checkmark"></span><span class="gender_span">male</span>
                 </div>
                 <div class="input_flex">
-                    <input type="radio" name="personal_gender" {{ ($personalpage->personal_gender=='female') ? 'checked="checked" ' : '' }} value="female" class="input_radio"><span class="gender_span">female</span>
+                    <input type="radio" name="personal_gender"
+                        {{ ($personalpage->personal_gender=='female') ? 'checked="checked" ' : '' }} value="female"
+                        class="input_radio"><span class="checkmark"></span><span class="gender_span">female</span>
                 </div>
-                <div class="input_flex"> <input type="radio" name="personal_gender" {{ ($personalpage->personal_gender=='gender neutral') ? 'checked="checked" ' : '' }} value="gender neutral" class="input_radio"><span
-                    class="gender_span">gender neutral</span>
+                <div class="input_flex"> <input type="radio" name="personal_gender"
+                        {{ ($personalpage->personal_gender=='gender neutral') ? 'checked="checked" ' : '' }}
+                        value="gender neutral" class="input_radio"><span class="checkmark"></span><span
+                        class="gender_span">gender neutral</span>
                 </div><br />
                 <span class="separate_span">age</span>
-                <input type="range" name="personal_age" min="18" max="99" placeholder="" value="{{ $personalpage->personal_age }}" class="input_range">
+                <div class="age_box_container">
+                    <input type="range" oninput="ageSlider(this.value)" name="personal_age" min="18" max="99"
+                        placeholder="" value="{{ $personalpage->personal_age }}" class="input_range">
+                    <span id="ageOutput"></span>
+                </div>
             </div>
             <div class="who_am_i">
-                <textarea class="text_area" placeholder="write something about yourself..." name="personal_info">{{ $personalpage->personal_info }}</textarea>
+                <textarea class="text_area" placeholder="write something about yourself..."
+                    name="personal_info">{{ $personalpage->personal_info }}</textarea>
                 <div class="see_personal_page">
                     <button class="button_personal_view">my personal page</button>
                     <button type="submit" class="button_save_changes">save my changes</button>
@@ -45,7 +60,7 @@
     <div class="profile_boxes">
         <div class="filter_flex">
             <div class="filter_name">I'm looking for...</div>
-            <form method="GET" action="/profilepages/{{ $personalpage->id }}">                
+            <form method="GET" action="/profilepages/{{ $personalpage->id }}">
                 {{-- to protect against cross site... --}}
                 @csrf
                 <div class="filter_options">
@@ -57,12 +72,15 @@
 
                         <input type="radio" name="filter_gender" value="female" class="input_radio">female
 
-                        <input type="radio" name="filter_gender" value="gender neutral" class="input_radio">gender neutral
+                        <input type="radio" name="filter_gender" value="gender neutral" class="input_radio">gender
+                        neutral
                     </div>
                     <span class="separate_span">age</span>
-                    <div><input type="range" name="filter_age" min="18" max="99" placeholder="" class="filter_age"></div>
+                    <div><input type="range" name="filter_age" min="18" max="99" placeholder="" class="filter_age">
+                    </div>
                     <span class="separate_span">distance</span>
-                    <div><input type="range" name="filter_distance" min="0" max="500" placeholder="" class="filter_age"></div>
+                    <div><input type="range" name="filter_distance" min="0" max="500" placeholder="" class="filter_age">
+                    </div>
                     <span class="separate_span">game genre</span>
                     <div class="genre_box">
                         <input list="genres" name="filter_genre" placeholder="choose a genre..." class="list_dropdown">
@@ -80,7 +98,7 @@
                         <button type="submit" class="find_matches_button">Find your match!</button>
                     </div>
                 </div>
-            </form>    
+            </form>
         </div>
         <div class="matches_flex">
             <div class="matches_card">
@@ -108,7 +126,7 @@
                 <div class="card_name_box"></div>
                 <div id="card_button_box"></div>
             </div>
-        </div> 
+        </div>
     </div>
     {{-- Start profile box 3 --}}
     <div class="profile_boxes">
