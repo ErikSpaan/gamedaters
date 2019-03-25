ee2110cebe9e6f3bf11bf5d793ef2602c23f0490
Philip Andes <philip__andes@hotmail.nl>
2019-03-25 12:55:28 +0100

    started chatpage, include chat, chat css

--- resources/views/chatpage.blade.php
+++ resources/views/chatpage.blade.php
@@ -1,7 +1,15 @@
 @extends('master')
 
 <!--Title on tab current page -->
-@section('title', 'chat')  
+@section('title', 'Chat') 
+
+<!--Name / logo landingpage -->
+@section('logo/menu')
+@include ('codeincludes/newmenu')
+
+<!-- Chat page -->
+{{-- @section('homepageLinks') --}}
+@include('codeincludes/chat')
 
 @section('content')
     <h1>chatpage</h1>
