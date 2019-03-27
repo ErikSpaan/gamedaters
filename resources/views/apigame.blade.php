7529a120b1033d991a6a908083d551d3ea9f4e25
RickButer <buter.rick@gmail.com>
2019-03-27 15:04:04 +0100

    games in database 3.27.2019 15:00

--- resources/views/apigame.blade.php
+++
@@ -1,6 +0,0 @@
-@foreach ($games as $game)
-<?php
-echo($game->cover . '</br>');
-echo('<img src="https://images.igdb.com/igdb/image/upload/t_cover_big/' . $game->cover . '.jpg"/>');
-?>
-@endforeach
