d6ae16e1149734631619ae4543449c20b118f6fc
RickButer <buter.rick@gmail.com>
2019-04-03 14:41:04 +0200

    finished change password button and started event page 2:40 4-3-2019

--- resources/views/eventpage.blade.php
+++ resources/views/eventpage.blade.php
@@ -3,18 +3,7 @@
 <!--Title on tab current page -->
 
 @section('title', 'events') 
-@section('content')
-<h1>eventpage</h1>
-<br> 
-@foreach ($events as $event)
-<li>
-    <a href="/events/{{ $event->id }}">{{ $event->event_name }}</a>
-    <a href="/addevent/{{ $event->id }}">join event</a> 
-    @if ($user->events()->where('event_id', $event -> id)->exists()) { aangemeld } 
-    @else { afgemeld } 
-    @endif
-</li>
-@endforeach
-<a href="{{$events->previousPageUrl()}}">previous page</a>
-<a href="{{$events->nextPageUrl()}}">next page</a> total events: {{ $events->total() }}
-@endsection
+@include('codeincludes/newmenu')
+@include('codeincludes/event')
+
+
