64b47e1bd5727c4afaa52a05a84c19240eba066d
ErikSpaan <email2erik@spamus.nl>
2019-04-04 14:00:12 +0200

    admin games 2

--- resources/views/games/edit.blade.php
+++ resources/views/games/edit.blade.php
@@ -26,43 +26,40 @@
 
      
         <div>
-            <label class="label" for="game_name">Game name</label>
+            <label class="event_label" for="game_name">Game name</label>
             <div class="event_field">
                 <input type="text" name="game_name" placeholder="Game name" value="{{ $game->game_name }}" autofocus required maxlength=60>
             </div>
         </div>
         <div>
-            <label class="label" for="game_genre">Game genre</label>
+            <label class="event_label" for="game_genre">Game genre</label>
             <div class="event_field">
                 <input type="text" name="game_genre" placeholder="Game genre" value="{{ $game->game_genre }}" required maxlength=40>
             </div>
         </div>
         <div class="event_field">
-            <label class="label" for="game_image_url">Game image name</label>
+            <label class="event_label" for="game_image_url">Game image name</label>
             <div>
                 <input type="text" name="game_image_url" placeholder="Image name" value="{{ $game->game_image_url }}" required maxlength=50>
             </div>
         </div>
         <div class="event_field">
-            <label class="label" for="game_description">Game Description</label>
+            <label class="event_label" for="game_description">Game Description</label>
             <div>
                 <textarea name="game_description" required maxlength=400>{{ $game->game_description }}</textarea>
             </div>
         </div>
         <div>
-       
-
-        <button type="submit">Update game</button>
-    </form>    
-
-    <br>
-<form method="POST" action="/games/{{ $game->id }}">       
-    {{-- {{ method_field('DELETE') }}
-    {{ csrf_field() }} --}}
-    @method('DELETE')
-    @csrf
-    <button type="submit">Delete game</button>
-</form>    
-<a href="/games/">Home</a>  
+        <button class="button_events_edit_los" type="submit">Update game</button>
+        <a href="\games"><button class="button_events_back">Back</button></a>    
+    </form> 
+    <form method="POST" action="/games/{{ $game->id }}">       
+        {{-- {{ method_field('DELETE') }}
+        {{ csrf_field() }} --}}
+        @method('DELETE')
+        @csrf
+        <button class="button_events_delete_los" type="submit">Delete game</button>
+    </form>  
+</div>   
 
 @endsection
