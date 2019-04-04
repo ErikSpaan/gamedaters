74f06ed0e9be1dc23c7b572986fefed970008dc0
Philip Andes <philip__andes@hotmail.nl>
2019-04-04 15:16:51 +0200

    test

--- resources/views/codeincludes/chat.blade.php
+++ resources/views/codeincludes/chat.blade.php
@@ -20,7 +20,7 @@
 
 @section('chat')
 
-<div class="container">
+<div class="container_chat">
     <div class="row justify-content-center">
         <div class="col-sm-12">
             <div class="card">
