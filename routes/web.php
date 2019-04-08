38a53ab06886743b49a99a5fc93d8a36ed46902e
ErikSpaan <email2erik@spamus.nl>
2019-04-08 11:38:27 +0200

    addevents button finished

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/admin', 'PageController@adminpage');
 Route::get('/apigame', 'GamesController@index');
 Auth::routes();
 
-Route::get('/addevent/{event_id}', 'EventsController@addEvent');
+// Route::get('/addevent/{event_id}', 'EventsController@addEvent');
 // Route::get('/deleteevent', 'EventsController@deleteEvent');
 // Route::get('/events/index', 'EventsController@show');
 Route::resource('events', 'EventsController');
@@ -45,3 +45,6 @@ Route::post('/getdates','AjaxController@index');
 Route::get('/personalpages',function(){ return view('personal_profile'); });
 Route::post('/findyourmatch','AjaxController@findyourmatch');
 
+//ajax route for join an event, saves in database and shows result of join
+//Route::get('/codeincludes',function(){ return view('eventpage'); });
+Route::post('/addevent','AjaxController@addevent');
